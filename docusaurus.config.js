943d0f8587ab35a4e91f00f5aa40b4220103d7c1
Haletha <haletha@gmail.com>
2026-08-18 00:35:47 -0500

    Configure custom domain for haletha.com

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,10 +20,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://creativeleej.github.io',
+  url: 'https://www.haletha.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/haletha-site-v2/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
